57aeeb4dbf7d361ac85875a947e1d9afdbd12f19
chenyou <1354329916@qq.com>
2020-11-04 15:48:13 +0800

    init

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,96 @@
+package entity;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * User实体类
+ * {
+ *   name:"abc",
+ *   age:18 随机数,
+ *   address:[
+ *   {
+ *    street:"xxx",
+ *    road:"xxx"
+ *   },
+ *   {
+ *   address:"unknown"
+ *   },
+ *   "unknown"],
+ *   ABC:{
+ *   "s":2,
+ *   "b":4
+ *   },
+ *   date: ISODate("2020-1-1")随机日期,
+ *   count:100<random int>
+ * }
+ */
+public class User {
+    private String name;
+    private Integer age;
+    private List<Object> address;
+    private Map<String,Object> abc;
+    private Date date;
+    private Integer count;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public List<Object> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<Object> address) {
+        this.address = address;
+    }
+
+    public Map<String, Object> getAbc() {
+        return abc;
+    }
+
+    public void setAbc(Map<String, Object> abc) {
+        this.abc = abc;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                ", abc=" + abc +
+                ", date=" + date +
+                ", count=" + count +
+                '}';
+    }
+}
---
+++ src/main/java/utils/UserUtil.java
@@ -0,0 +1,48 @@
+package utils;
+
+import entity.User;
+
+import java.util.*;
+
+/**
+ * User工具类
+ */
+public class UserUtil {
+    /**
+     * 获取一个有值的user
+     */
+    public static User getUser(){
+        User user = new User();
+        //姓名
+        user.setName("abc");
+        //年龄
+        Random random = new Random();
+        user.setAge(random.nextInt(100));
+        //address
+        List address = new ArrayList();
+        //添加address里的streetMap
+        Map streetMap = new HashMap();
+        streetMap.put("street","xxx");
+        streetMap.put("rod","xxx");
+        address.add(streetMap);
+        //添加address里的addressMap
+        Map addressMap = new HashMap();
+        addressMap.put("address","unknown");
+        address.add(addressMap);
+        //添加address里的unknown
+        address.add("unknown");
+        user.setAddress(address);
+        //ABC
+        Map abc = new HashMap();
+        abc.put("s",2);
+        abc.put("b",4);
+        user.setAbc(abc);
+        //date
+        Date randomDate = DateUtil.getRandomDate("2018-01-01", "2020-01-01");
+        user.setDate(randomDate);
+        //count
+        user.setCount(random.nextInt(200));
+
+        return user;
+    }
+}
